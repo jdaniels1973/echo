896a49edc3a6ef655a0a491e6d6c5018f74f5ba0
jdaniels1973 <118066190+jdaniels1973@users.noreply.github.com>
2022-11-12 22:17:19 +0000

    Created helloworld server

---
+++ replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+	deps = [
+		pkgs.less
+  pkgs.wget
+  pkgs.nodejs-18_x
+        pkgs.nodePackages.typescript-language-server
+        pkgs.nodePackages.yarn
+        pkgs.replitPackages.jest
+	];
+}
